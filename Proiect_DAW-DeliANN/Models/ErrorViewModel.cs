1f546c058d7ddda4b3c0c1b52bcc8c57c7fa95d7
Dan Delian <dandelian18@gmail.com>
2024-11-18 20:01:29 +0200

    Created the Project

---
+++ Proiect_DAW-DeliANN/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proiect_DAW_DeliANN.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
