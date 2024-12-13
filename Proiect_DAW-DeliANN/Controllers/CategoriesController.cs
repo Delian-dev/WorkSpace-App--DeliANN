eac32420352b7776a8b2eb090a77837f0e00e4f2
Dan Delian <dandelian18@gmail.com>
2024-12-13 16:58:13 +0200

    Finished controller actions and added views

--- Proiect_DAW-DeliANN/Controllers/CategoriesController.cs
+++ Proiect_DAW-DeliANN/Controllers/CategoriesController.cs
@@ -1,12 +1,109 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Proiect_DAW_DeliANN.Data;
+using Proiect_DAW_DeliANN.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Proiect_DAW_DeliANN.Controllers
 {
+    [Authorize(Roles = "Admin")] //admin only
     public class CategoriesController : Controller
     {
-        public IActionResult Index()
+        //PASUL 10: useri si roluri
+        private readonly ApplicationDbContext db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public CategoriesController(
+        ApplicationDbContext context,
+        UserManager<ApplicationUser> userManager,
+        RoleManager<IdentityRole> roleManager
+        )
+        {
+            db = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index() //la cezara e de tip ActionResult dar din ce inteleg e doar o forma default de IActionResult
+        {
+            //adaugarea in ViewBag al unui mesaj pentru afisare, daca avem ceva de transmis (adaugare/modificare/stergere categorie)
+            if (TempData.ContainsKey("message"))
+            {
+                ViewBag.message = TempData["message"].ToString();
+            }
+
+            var categories = from category in db.Categories
+                             orderby category.CategoryName
+                             select category;
+            ViewBag.Categories = categories; //mergem cu toate categoriile in view-ul de afisare
+            return View();
+        }
+
+        public IActionResult Show(int id)
+        {
+            Category category = db.Categories.Find(id); //mergem in view cu categoria specificata
+            return View(category);
+        }
+
+        public IActionResult New() //HTTPGET implicit
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult New(Category cat)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(cat);
+                db.SaveChanges();
+                TempData["message"] = "Categoria a fost adaugata";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(cat);
+            }
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Category category = db.Categories.Find(id);
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, Category requestCategory)
+        {
+            Category category = db.Categories.Find(id);
+
+            if (ModelState.IsValid)
+            {
+                category.CategoryName = requestCategory.CategoryName;
+                db.SaveChanges();
+                TempData["message"] = "Categoria a fost modificata!";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(requestCategory);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id) {
+            Category category = db.Categories.Include("Workspaces")
+                                             .Include("Workspaces.Channels")
+                                             .Include("Workspaces.Channels.Posts")
+                                             .Include("Workspaces.Channels.Posts.Reactions")
+                                             .Where(c => c.CategoryId == id)
+                                             .First();
+            db.Categories.Remove(category);
+
+            TempData["message"] = "Categoria a fost stearsa";
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
--- Proiect_DAW-DeliANN/obj/Debug/net8.0/Proiect_DAW-DeliANN.AssemblyInfo.cs
+++ Proiect_DAW-DeliANN/obj/Debug/net8.0/Proiect_DAW-DeliANN.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proiect_DAW-DeliANN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49a4ab5b1a44ef878914b99107705c7aa5fbdc19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eaf6cb3ff726de2d5b0a3c946f07092622340e3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proiect_DAW-DeliANN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proiect_DAW-DeliANN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
