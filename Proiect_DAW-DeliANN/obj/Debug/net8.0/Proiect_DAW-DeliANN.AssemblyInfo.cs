cbc71903d45583d4dd684b2034b83e05f9eb8702
Dan Delian <dandelian18@gmail.com>
2024-12-14 21:31:26 +0200

    made the show new edit and delete methods work on Workspaces

--- Proiect_DAW-DeliANN/obj/Debug/net8.0/Proiect_DAW-DeliANN.AssemblyInfo.cs
+++ Proiect_DAW-DeliANN/obj/Debug/net8.0/Proiect_DAW-DeliANN.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proiect_DAW-DeliANN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+282713aab91bcb6bd893825b870a6b4ebabf676b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22c89751de2abfa652508e7fc5e5ddb53c512b84")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proiect_DAW-DeliANN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proiect_DAW-DeliANN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
